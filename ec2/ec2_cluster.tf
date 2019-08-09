7bb5d0d78c27b76e854367a7e4fc9c843bd09d52
marcelpmd <marcel.kowynia@charter.com>
2019-08-09 08:54:51 -0600

    fadsfd

--- ec2/ec2_cluster.tf
+++ ec2/ec2_cluster.tf
@@ -24,11 +24,6 @@ resource "aws_s3_bucket" "etcd_backup_store" {
   }
 }
 
-# resource "aws_key_pair" "ssh_key" {
-#   key_name   = "${var.cluster_name}-cluster"
-#   public_key = "${file(var.ssh_public_key_file)}"
-# }
-
 data "aws_subnet_ids" "available" {
   vpc_id = "${var.vpc_id}"
   filter {
@@ -133,48 +128,55 @@ resource "rancher2_node_template" "worker_nodetemplate" {
     vpc_id = "${var.vpc_id}"
     zone = substr("${data.aws_subnet.selected[count.index].availability_zone}", 9, 1)
     use_private_address = "true"
+    tags = "node-role.kubernetes.io/worker-web,true"
   }
+  labels = {
+    "node-role.kubernetes.io/worker-web" = "true"
+  }
+
 }
 
 resource "rancher2_node_pool" "control_plane_node_pool" {
   count = "${var.control_plane_count}"
   cluster_id =  "${rancher2_cluster.cluster.id}"
   name = "${var.cluster_name}-cp-node-pool-az${count.index}"
-  hostname_prefix =  "${var.cluster_name}-cp"
+  hostname_prefix =  "${var.cluster_name}-cp-${count.index}"
   node_template_id = "${rancher2_node_template.control_plane_nodetemplate[count.index].id}"
   quantity = 1
   control_plane = true
   etcd = false
   worker = false
-  depends_on = "${rancher2_node_pool.etcd_node_pool}"
+  depends_on = [rancher2_node_pool.etcd_node_pool]
 }
 
 resource "rancher2_node_pool" "etcd_node_pool" {
   count = "${var.etcd_count}"
   cluster_id =  "${rancher2_cluster.cluster.id}"
   name = "${var.cluster_name}-etcd-node-pool-az${count.index}"
-  hostname_prefix =  "${var.cluster_name}-etcd"
+  hostname_prefix =  "${var.cluster_name}-etcd-${count.index}"
   node_template_id = "${rancher2_node_template.control_plane_nodetemplate[count.index].id}"
   quantity = 1
   control_plane = false
   etcd = true
   worker = false
+  depends_on = [rancher2_node_template.control_plane_nodetemplate,aws_security_group.cluster_sg]
+
 }
 
 resource "rancher2_node_pool" "worker_node_pool" {
   count = "${var.worker_count}"
   cluster_id =  "${rancher2_cluster.cluster.id}"
   name = "${var.cluster_name}-worker-node-pool-az${count.index}"
-  hostname_prefix =  "${var.cluster_name}-worker"
+  hostname_prefix =  "${var.cluster_name}-worker-${count.index}"
   node_template_id = "${rancher2_node_template.worker_nodetemplate[count.index].id}"
-  labels = {
-    "node-role.kubernetes.io/worker-web" = "true"
-  }
   quantity = 1
   control_plane = false
   etcd = false
   worker = true
-  depends_on = "${rancher2_node_pool.control_plane_node_pool}"
+  depends_on = [rancher2_node_pool.control_plane_node_pool,rancher2_node_template.worker_nodetemplate]
+  labels = {
+    "node-role.kubernetes.io/worker-web" = "true"
+  }
 }
 
 resource "rancher2_etcd_backup" "cluster-backups" {
